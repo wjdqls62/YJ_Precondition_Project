4ca2a80085f4894f20ed20429a3aed8b7e2d29cc
wjdqls62 <wjdqls62@naver.com>
2017-04-18 22:17:18 +0900

    * 기기정보 표시정보 추가  - 제조사, 칩셋, SW버전, 안드로이드 버전
     * 기존 기기정보 xml 삭제

--- app/src/main/java/prc/yjsys/com/reliabilitypreconditionforart/Fragment/Information_Fragment.java
+++ app/src/main/java/prc/yjsys/com/reliabilitypreconditionforart/Fragment/Information_Fragment.java
@@ -12,6 +12,10 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import prc.yjsys.com.reliabilitypreconditionforart.R;
 import prc.yjsys.com.reliabilitypreconditionforart.Utility.DeviceManager;
 
@@ -20,7 +24,11 @@ import prc.yjsys.com.reliabilitypreconditionforart.Utility.DeviceManager;
  */
 public class Information_Fragment extends PreferenceFragment{
 
-    Preference dModelName, dIMEI, dPhoneNumber, dOperater = null;
+    Process process = null;
+    BufferedReader bufferedReader = null;
+    String GETPROP_EXECUTABLE_PATH = "/system/bin/getprop";
+
+    Preference dModelName, dIMEI, dPhoneNumber, dBoard, dAndroidVersion,dSWVersion, dManufacturer = null;
     DeviceManager DM = null;
 
     public static Fragment newInstance(){
@@ -45,7 +53,49 @@ public class Information_Fragment extends PreferenceFragment{
 
         dPhoneNumber = findPreference("devinfo_title_phonenumber");
         dPhoneNumber.setSummary(DM.getPhoneNumber());
+
+        dBoard = findPreference("devinfo_title_board");
+        dBoard.setSummary(readProp("ro.product.board"));
+
+        dAndroidVersion = findPreference("devinfo_title_android_version");
+        dAndroidVersion.setSummary(readProp("ro.build.version.release"));
+
+        dSWVersion = findPreference("devinfo_title_swversion");
+        dSWVersion.setSummary(readProp("ro.lge.swversion"));
+
+        dManufacturer = findPreference("devinfo_title_manufacturer");
+        dManufacturer.setSummary(readProp("ro.product.manufacturer"));
     }
+
+    public String readProp(String propName) {
+        try {
+            process = new ProcessBuilder().command(GETPROP_EXECUTABLE_PATH, propName).redirectErrorStream(true).start();
+            bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line = bufferedReader.readLine();
+
+            if (line == null) {
+                line = "";
+            }
+            return line;
+        } catch (IOException e) {
+            return "";
+        } finally {
+            if (bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                } catch (IOException e) {
+                    return "";
+                }
+            }
+            if (process != null) {
+                process.destroy();
+            }
+        }
+    }
+
+
+
+
 }
 
 
